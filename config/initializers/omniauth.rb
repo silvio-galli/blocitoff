f1f52c61e53463d092962c5bf96b4eeff69bc8cf
silvio-galli <silvio.galli@gmail.com>
2016-04-21 23:52:58 +0200

    Added omiauth-google-oauth2 user sign up/in/out

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
   provider :facebook, Rails.application.secrets.facebook_api_key, Rails.application.secrets.facebook_api_secret, scope: 'public_profile', info_fields: 'id,name,link'
+  provider :google_oauth2, Rails.application.secrets.google_api_key, Rails.application.secrets.google_api_secret, scope: 'profile', image_aspect_ratio: 'square', image_size: 32, access_type: 'online'
 end
