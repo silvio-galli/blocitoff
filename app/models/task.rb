671401830618e42a25854d364d6f5b85807fed6d
silvio-galli <silvio.galli@gmail.com>
2016-05-10 22:22:56 +0200

    Validates that end_date is bigger then Time.now and refactored seeds

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :user
 
   validates :title, presence: true, length: { minimum: 3 }
-  validates :expected_end_date, presence: true
+  validates :expected_end_date, presence: true, if: :ends_after_now?
 
   after_initialize { self.completed ||= false }
   after_initialize { self.uncompleted ||= false }
@@ -12,4 +12,8 @@ class Task < ActiveRecord::Base
   scope :completed, -> { where(completed: true) }
   scope :deleted, -> { where(deleted: true) }
   scope :uncompleted, -> { where(uncompleted: true) }
+
+  def ends_after_now?
+    expected_end_date >= DateTime.now.end_of_day
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 50.times do
   task = Task.create!(
     title: Faker::Lorem::sentence,
-    expected_end_date: Faker::Time.between(DateTime.now - 30, DateTime.now + 30),
+    created_at: Faker::Time.between(DateTime.now - 30, DateTime.now - 1),
+    expected_end_date: Faker::Time.between(DateTime.now, DateTime.now + 30),
     user_id: "1",
   )
 end
