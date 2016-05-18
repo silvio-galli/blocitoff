9b59032c54efea275133cfa4e705ac10590d843d
silvio-galli <silvio.galli@gmail.com>
2016-05-18 19:23:19 +0200

    Deleted problematic migration for PG

--- db/migrate/20160511232718_change_tasks_date_fields_column_type.rb
+++
@@ -1,8 +0,0 @@
-class ChangeTasksDateFieldsColumnType < ActiveRecord::Migration
-  def change
-    change_table :tasks do |t|
-      t.change :expected_end_date, :datetime
-      t.change :actual_end_date, :datetime
-    end
-  end
-end
