3e1cfe4d97b52a1fb024b0513280073384598793
silvio-galli <silvio.galli@gmail.com>
2016-05-19 02:50:15 +0200

    Added figaro gem to manage keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
   # provider :facebook, Rails.application.secrets.facebook_api_key, Rails.application.secrets.facebook_api_secret, scope: 'public_profile', info_fields: 'id,name,link'
-  provider :google_oauth2, Rails.application.secrets.google_api_key, Rails.application.secrets.google_api_secret
+  provider :google_oauth2, Figaro.env.google_api_key, Figaro.env.google_api_secret
   #facebook
   #google scope: 'profile', image_aspect_ratio: 'square', image_size: 32, access_type: 'online'
 end
